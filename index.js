9d026e87cad62d3518b1e36fd4b22550bff96f7a
MdShafiqulSaymon <mdshafiqulsaymon@gmail.com>
2024-08-13 14:03:53 -0400

    first Day Complete

---
+++ index.js
@@ -0,0 +1,20 @@
+// JavaScript for automatic slider
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); // Change slide every 5 seconds
